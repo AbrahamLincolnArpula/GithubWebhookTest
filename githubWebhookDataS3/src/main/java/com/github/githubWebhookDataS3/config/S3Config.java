ee96861807b6fce4384b70597cae7f67dde285bf
ABRAHAM LINCOLN <ABRAHAM LINCOLN@LAPTOP-KUNSANB6>
2024-08-17 02:27:27 +0530

    Add refreshing the credentials automatically

--- githubWebhookDataS3/src/main/java/com/github/githubWebhookDataS3/config/S3Config.java
+++ githubWebhookDataS3/src/main/java/com/github/githubWebhookDataS3/config/S3Config.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
 import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
 
@@ -17,26 +18,42 @@ public class S3Config {
     @Bean
     @Profile("prod")
     public S3Client s3Client() {
+		/*
+		 * StsClient stsClient = StsClient.builder() .region(Region.AP_SOUTH_1)
+		 * .build();
+		 * 
+		 * AssumeRoleRequest assumeRoleRequest = AssumeRoleRequest.builder()
+		 * .roleArn("arn:aws:iam::590183986028:role/githubS3Role")
+		 * .roleSessionName("sessionName") .build();
+		 * 
+		 * AssumeRoleResponse assumeRoleResponse =
+		 * stsClient.assumeRole(assumeRoleRequest);
+		 * 
+		 * AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
+		 * assumeRoleResponse.credentials().accessKeyId(),
+		 * assumeRoleResponse.credentials().secretAccessKey(),
+		 * assumeRoleResponse.credentials().sessionToken() );
+		 * 
+		 * return S3Client.builder() .region(Region.AP_SOUTH_1)
+		 * .credentialsProvider(StaticCredentialsProvider.create(sessionCredentials))
+		 * .build();
+		 */
+    	
         StsClient stsClient = StsClient.builder()
-                .region(Region.AP_SOUTH_1)
+                .region(Region.AP_SOUTH_1)  // Replace with your region
                 .build();
 
         AssumeRoleRequest assumeRoleRequest = AssumeRoleRequest.builder()
-                .roleArn("arn:aws:iam::590183986028:role/githubS3Role")
+                .roleArn("arn:aws:iam::590183986028:role/githubS3Role")  // Replace with your role ARN
                 .roleSessionName("sessionName")
                 .build();
 
-        AssumeRoleResponse assumeRoleResponse = stsClient.assumeRole(assumeRoleRequest);
-
-        AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-                assumeRoleResponse.credentials().accessKeyId(),
-                assumeRoleResponse.credentials().secretAccessKey(),
-                assumeRoleResponse.credentials().sessionToken()
-        );
-
         return S3Client.builder()
                 .region(Region.AP_SOUTH_1)
-                .credentialsProvider(StaticCredentialsProvider.create(sessionCredentials))
+                .credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
+                    .refreshRequest(assumeRoleRequest)
+                    .stsClient(stsClient)
+                    .build())
                 .build();
     }
 
